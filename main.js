aba4dee4b3b91f10929b4b387d9fbb9e11124126
BineetKarmakar <83464472+BineetKarmakar@users.noreply.github.com>
2021-08-26 11:39:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,63 +1,39 @@
-var last_position_of_x, last_position_of_y;
+canvas=document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
-
-    var width=screen.width;
-    var new_width=screen.width-80;
-    var new_height=screen.height-100;
-    if(width<992){
-        canvas.width=new_width;
-        canvas.height=new_height;
-        document.body.style.overflow="hidden";
-    }
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth=1;
+ctx.rect(150,143,430,200);
+ctx.stroke();
 
 
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth=5;
+ctx.arc(260,210,40,0,2 * Math.PI);
+ctx.stroke();
 
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth=5;
+ctx.arc(360,210,40,0,2 * Math.PI);
+ctx.stroke();
 
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth=5;
+ctx.arc(460,210,40,0,2 * Math.PI);
+ctx.stroke();
 
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth=5;
+ctx.arc(310,250,40,0,2 * Math.PI);
+ctx.stroke();
 
-    canvas.addEventListener("touchstart", my_mousedown);
-    
-    function my_mousedown(e)
-    {
-        //Addictonal Activity start
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        //Addictonal Activity ends
-        last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
-        last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
-        
-    }
-
-    
-
-
-
-    canvas.addEventListener("touchmove", my_mousemove);
-    function my_mousemove(e)
-    {
-
-         current_position_of_mouse_x = e.touches[0].clientX - canvas.offsetLeft;
-         current_position_of_mouse_y = e.touches[0].clientY - canvas.offsetTop;
-
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke();
-        
-
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
-    }
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth=5;
+ctx.arc(410,250,40,0,2 * Math.PI);
+ctx.stroke();
